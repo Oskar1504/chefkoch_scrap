fe9a45b14964b04614019c2f174e0455903ebf62
Oskar Pester <oskar.pester2@gmail.com>
2022-01-10 18:39:47 +0100

    added route so u can receive evfery recipe from today

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require('express')
 const HTMLParser = require('node-html-parser');
 const axios = require("axios");
 const fs = require("fs");
+const Log = require("./helper/Log");
 
 const app = express()
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    Log.request(req.path)
+    next()
+})
 
 app.get('/api/getWeekRecipes', async function (req, res) {
 
@@ -34,7 +39,7 @@ app.get('/api/getWeekRecipes', async function (req, res) {
     }
 
     //check if wekk already scrapped
-    if(!fs.existsSync(__dirname + `/data/${cat_input}_${week_input}.json`)) {
+    if(!fs.existsSync(__dirname + `/data/${cat_input}/${week_input}.json`)) {
 
         let hostUrl = "https://www.chefkoch.de/wochenplan",
             url = `${hostUrl}/${cat_input}/${week_input}`
@@ -71,16 +76,19 @@ app.get('/api/getWeekRecipes', async function (req, res) {
                     recipes.push({
                         title: day.querySelectorAll(".wr-card__title")[0].text,
                         day: sanitizeText(day.querySelectorAll(".wr-card__day")[0].text),
+                        categorie: cat_input,
+                        day_num: dayToNum(sanitizeText(day.querySelectorAll(".wr-card__day")[0].text)),
                         link: recipe_link,
                         duration: sanitizeText(day.querySelectorAll(".wr-meta__item--clock")[0].text),
-                        more_data: more_data
+                        ingredients: more_data.ingredients,
+                        description: more_data.description
                     })
 
                     await new Promise(resolve => setTimeout(resolve, 1000));
                 }
 
                 // store data
-                fs.writeFileSync(__dirname + `/data/${cat_input}_${week_input}.json`, JSON.stringify(recipes, null, 4))
+                fs.writeFileSync(__dirname + `/data/${cat_input}/${week_input}.json`, JSON.stringify(recipes, null, 4))
 
                 res.json({
                     status: 200,
@@ -98,10 +106,39 @@ app.get('/api/getWeekRecipes', async function (req, res) {
         infos.push("Week already scrapped. Using local storage")
         res.json({
             status: 200,
-            recipes: JSON.parse(fs.readFileSync(__dirname + `/data/${cat_input}_${week_input}.json`)),
+            recipes: JSON.parse(fs.readFileSync(__dirname + `/data/${cat_input}/${week_input}.json`)),
             infos: infos
         })
     }
+})
+
+app.get('/api/getTodaysRecipes', async function (req, res) {
+
+    let infos = [],
+        output = [],
+        now = new Date(),
+        today = now.getDay()-1, // -1 since day works as index for array monday is 0 but getday return 1
+        onejan = new Date(now.getFullYear(), 0, 1),
+        week_actual = Math.ceil((((now.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7)-1;
+
+
+    // categorie validation
+    let valid_cat = ["schnelle-alltagskueche", "gesunde-ernaehrung","vegetarische-vielfalt","low-carb"]
+
+    for(let cat of valid_cat){
+        // if cat data exist from this week
+        if(fs.existsSync(__dirname + `/data/${cat}/${week_actual}.json`)) {
+            let recipes = JSON.parse(fs.readFileSync(__dirname + `/data/${cat}/${week_actual}.json`))
+            output.push(recipes[today])
+        }else{
+            infos.push(`Recipes from ${cat} arent scrapped for this week (${week_actual})`)
+        }
+    }
+    res.json({
+        status: 200,
+        recipes: output,
+        infos: infos
+    })
 })
 
 app.get('/api/testParams', async function (req, res) {
@@ -161,4 +198,33 @@ console.log('\napp started. http://localhost:42069');
 
 function sanitizeText(text){
     return text.replace(/\s\s+/g, '');
+}
+
+function dayToNum(day) {
+    // need to use include since days sometimes include whitespace or other random character
+    switch (true) {
+        case /Montag/.test(day):
+            return 1
+            break
+        case /Dienstag/.test(day):
+            return 2
+            break
+        case /Mittwoch/.test(day):
+            return 3
+            break
+        case /Donnerstag/.test(day):
+            return 4
+            break
+        case /Freitag/.test(day):
+            return 5
+            break
+        case /Samstag/.test(day):
+            return 6
+            break
+        case /Sonntag/.test(day):
+            return 7
+            break
+        default:
+            return 1
+    }
 }
